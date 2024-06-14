651bc9e3e430c3d205a45a12b2fd2361d97580da
jhreyes483 <jhreyes483@misena.edu.co>
2024-06-14 15:02:30 -0500

    ts @interfece-and-destructure

--- 01)-ts/bases_ts/project/01-typescript-intro/src/main.ts
+++ 01)-ts/bases_ts/project/01-typescript-intro/src/main.ts
@@ -1,6 +1,10 @@
 import './style.css'
-import './topics/01-basic-types'
-import './topics/02-object-interface';
+//import './topics/01-basic-types'
+//import './topics/02-object-interface';
+//import './topics/03-functions-and-interface';
+//import './topics/04-homework-types';
+import './topics/05-basic-destructuring'
+
 //import { setupCounter } from './counter.ts'
 
 const app = document.querySelector<HTMLDivElement>('#app');
--- 01)-ts/bases_ts/project/01-typescript-intro/src/topics/03-functions-and-interface.ts
+++ 01)-ts/bases_ts/project/01-typescript-intro/src/topics/03-functions-and-interface.ts
@@ -11,30 +11,34 @@ function multiply(firtNumber: number, secondNumber?: number, base: number = 2){
     return firtNumber * base;
 }
 
-
 interface Character {
     name : string;
     hp: number;
     showHp: ()=> void;
 }
 
+const healCharacter = (character: Character, amount: number) =>{
+    character.hp += amount
+}
 
 const strider: Character = {
     name : 'Strider',
     hp: 50,
     showHp(){
-        console.log('ddd')
+        console.log(`Puntos de vida  ${this.hp}`)
     }
 }
 
-const heal = (character : Character, amount) =>{
-    character.name : String;
-    character.
-
-}
 
 const result : number = addNumbers(1,2);
 const result2 : string = addNumbersArrow(1,2);
 const result3 : number = multiply(5);
+
+strider.showHp()
+healCharacter(strider, 10);
+strider.showHp()
+
+
+console.log('function-and-interface', {result, result2, result3})
 
 export {};
