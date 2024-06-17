98864f355a9562846bd975c436ae8f6d67e4c58e
jhreyes483 <jhreyes483@misena.edu.co>
2024-06-17 12:47:55 -0500

    @angular -> ngFor

--- 02)-angular/01-bases/src/app/heroes/list/list.component.ts
+++ 02)-angular/01-bases/src/app/heroes/list/list.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+  public heroes = [
+    {
+      id: 1,
+      name: "spiderman",
+    },
+    {
+      id: 2,
+      name: "Ironman",
+    },
+    {
+      id: 3,
+      name: "Hulk",
+    },
+    {
+      id: 4,
+      name: "Thor",
+    }
+  ]
+
 
 }
