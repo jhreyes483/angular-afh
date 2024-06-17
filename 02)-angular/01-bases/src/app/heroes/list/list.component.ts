5eafb3d927e29f782ca94273eb8fa92f3b6f54d9
jhreyes483 <jhreyes483@misena.edu.co>
2024-06-17 14:32:29 -0500

    @angular ->ngIF and ng-template

--- 02)-angular/01-bases/src/app/heroes/list/list.component.ts
+++ 02)-angular/01-bases/src/app/heroes/list/list.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 
+class Hero{
+  constructor(
+    public id: number,
+    public name : string
+  ){
+
+  }
+}
+
 @Component({
   selector: 'app-heroes-list',
   templateUrl: './list.component.html',
@@ -24,6 +33,18 @@ export class ListComponent {
       name: "Thor",
     }
   ]
+  public  deleteHero : any;
+
+
+  constructor(){
+    this.deleteHero = new Hero (0,'');
+  }
+
+
+
+  removeLastHero() :void{
+    this.deleteHero = this.heroes.pop();
+  }
 
 
 }
