3f2cd64a6cbea82ae118c36bcf0a910623e03bcc
jhreyes483 <jhreyes483@misena.edu.co>
2024-06-18 18:26:40 -0500

    @angular @gifapp -> creacion de componente sidebar

--- 02)-angular/02-gif-app/src/app/app.module.ts
+++ 02)-angular/02-gif-app/src/app/app.module.ts
@@ -4,13 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+/* modules */
+import { GifsModule } from './gifs/gifs.module';
+import { SharedModule } from './shared/shared.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    /** nodules  */
+    GifsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
