c11dcebd77b740b46af0f1932706e7cb11be3321
jhreyes483 <jhreyes483@misena.edu.co>
2024-06-19 16:15:27 -0500

    end gift-app

--- 02)-angular/02-gif-app/src/app/gifs/components/card/card.component.ts
+++ 02)-angular/02-gif-app/src/app/gifs/components/card/card.component.ts
@@ -1,17 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Gif } from '../../interfaces/gifs.intefaces';
 
 @Component({
   selector: 'gifs-card',
-  templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  templateUrl: './card.component.html'
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+
   @Input()
   public gif!: Gif;
 
 
-
-
+  ngOnInit(): void {
+    if ( !this.gif ) throw new Error('Gif property is required');
+  }
 
 }
