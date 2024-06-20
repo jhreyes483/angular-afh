d41134b1ebc8ea5a7650b0b32baf4d8d957b9e72
jhreyes483 <jhreyes483@misena.edu.co>
2024-06-20 14:09:34 -0500

    regfactor botones de region y uso de ngClass

--- 02)-angular/03-country-app/src/app/countries/page/by-region-page/by-region-page.component.ts
+++ 02)-angular/03-country-app/src/app/countries/page/by-region-page/by-region-page.component.ts
@@ -8,21 +8,23 @@ import { CountryService } from '../../services/countries.services';
   styles: ``
 })
 export class ByRegionPageComponent {
+
   public countries: Country[] = [];
-  public title : string ='Por region';
+  public title : string ='Por región';
   public placeholder: string = 'busqueda por region';
+  public regions: string[] = ['Africa', 'America','Asia', 'Europe', 'Oceania',]
+  public selectedRegion?: string;
 
   constructor(
     private _countryService: CountryService
   ) { }
 
   searchByTerm(value: string): void {
+    this.selectedRegion = value;
     this._countryService.searchRegion(value)
       .subscribe(countries => {
         this.countries = countries;
-
       })
-
     console.log('serchCapital -->', value);
   }
 }
