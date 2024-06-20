e4840f030ad2442e17446aee4a3c193bc491f86e
jhreyes483 <jhreyes483@misena.edu.co>
2024-06-20 13:43:52 -0500

    complemento de cierre para el input con espera

--- 02)-angular/03-country-app/src/app/shared/components/search-box/search-box.component.ts
+++ 02)-angular/03-country-app/src/app/shared/components/search-box/search-box.component.ts
@@ -1,11 +1,17 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit, OnDestroy } from '@angular/core';
+import { Subject, Subscription, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styleUrl: './search-box.component.css'
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit, OnDestroy{
+
+  /***tipo de obserbable  */
+  private debouncer : Subject<string> = new Subject<string>(); /*  waitAndSend */
+  private debouncerSuscription?: Subscription;
+
   @Input()
   public placeholder: string = '';
 
@@ -17,5 +23,27 @@ export class SearchBoxComponent {
     console.log('emitio evento -> ', value)
   }
 
+  ngOnInit(): void {
+    this.waitAndSend()
+  }
+
+  ngOnDestroy(): void {
+    /** cierra el obserbable  */
+    this.debouncerSuscription?.unsubscribe()
+  }
+
+  onKeyPress( searchTern : string ){
+    this.debouncer.next(searchTern)
+  }
+
+  waitAndSend(){
+    this.debouncerSuscription = this.debouncer
+    .pipe(
+      debounceTime(500)
+    )
+    .subscribe(value =>{
+        this.onValue.emit(value);
+    })
+  }
 
 }
