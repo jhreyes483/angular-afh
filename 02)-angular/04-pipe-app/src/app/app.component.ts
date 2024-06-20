a0505660ea8fddf0b2144d1be9c05b9398bd3675
jhreyes483 <jhreyes483@misena.edu.co>
2024-06-20 17:16:41 -0500

    instalación de primeNg

--- 02)-angular/04-pipe-app/src/app/app.component.ts
+++ 02)-angular/04-pipe-app/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'pipes-app';
+export class AppComponent implements OnInit {
+  public title = 'JavIeR ReYes';
+
+  constructor(
+    private primeNGConfig: PrimeNGConfig
+  ){}
+  ngOnInit(): void {
+    this.primeNGConfig.ripple = true
+  }
+
 }
