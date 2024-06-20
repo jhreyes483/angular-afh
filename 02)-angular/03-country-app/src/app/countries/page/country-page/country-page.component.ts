ccada86f5c508bb58c8bd3d023c38b18133663a3
jhreyes483 <jhreyes483@misena.edu.co>
2024-06-20 12:02:39 -0500

    se crea detalle de país

--- 02)-angular/03-country-app/src/app/countries/page/country-page/country-page.component.ts
+++ 02)-angular/03-country-app/src/app/countries/page/country-page/country-page.component.ts
@@ -2,6 +2,7 @@ import { Component , OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { CountryService } from '../../services/countries.services';
 import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
@@ -9,14 +10,12 @@ import { switchMap } from 'rxjs';
   styles: ``
 })
 export class CountryPageComponent implements OnInit{
-  private id : string = '';
+  public country?: Country;
   constructor(
     private _route: ActivatedRoute,
     private _router: Router,
     private _countryService: CountryService,
-  ){
-
-  }
+  ){}
 
   ngOnInit(): void {
     this._route.params
@@ -24,11 +23,8 @@ export class CountryPageComponent implements OnInit{
       switchMap(({id})  =>this._countryService.searchCountryByAlphaCode(id)),
     )
     .subscribe(country =>{
-      if(!country){
-        return this._router.navigateByUrl('');
-      }
-      console.log('tenemos un pais');
-      return;
+      if(!country) return this._router.navigateByUrl('');
+      return this.country = country;
     })
   }
 }
