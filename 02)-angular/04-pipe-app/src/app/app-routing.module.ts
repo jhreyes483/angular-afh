f38c775fa8d40b0200041323ad2aa96d8bac6d9b
jhreyes483 <jhreyes483@misena.edu.co>
2024-06-20 18:33:12 -0500

    OJO: EJEMPLO DE IMPLEMENTACION RUTAS HIJAS

--- 02)-angular/04-pipe-app/src/app/app-routing.module.ts
+++ 02)-angular/04-pipe-app/src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {  }
+  {
+    path: '',
+    /** carga de rutas hijas */
+    loadChildren: () => import('./products/products.module').then(m => m.ProductsModule)
+   }
 ];
 
 @NgModule({
--- 02)-angular/04-pipe-app/src/app/products/products-routing.module.ts
+++ 02)-angular/04-pipe-app/src/app/products/products-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
+import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BaseAudioContext
+  },
+  {
+    path: 'numbers',
+    component: NumbersPageComponent
+  },
+  {
+    path: 'uncommon',
+    component: UncommonPageComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
