b8415073cff373cf6f69b62be8c20952713cd7e2
jhreyes483 <jhreyes483@misena.edu.co>
2024-06-20 18:35:40 -0500

    se agrega coment de vide carga de rutas hijas

--- 02)-angular/04-pipe-app/src/app/app-routing.module.ts
+++ 02)-angular/04-pipe-app/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   {
     path: '',
     /** carga de rutas hijas */
+    // https://www.udemy.com/course/angular-fernando-herrera/learn/lecture/37397546#overview
     loadChildren: () => import('./products/products.module').then(m => m.ProductsModule)
    }
 ];
