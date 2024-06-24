743d882a2558374dbbf0e4d9c4ffcd023cf0bd8d
jhreyes483 <jhreyes483@misena.edu.co>
2024-06-24 14:23:30 -0500

    OJO ARRAYFORMS CREAR INPUTS DIMICAMENTE DESDE FRONT

--- 02)-angular/06-forms-app/src/app/reactive/pages/dinamic-page/dinamic-page.component.ts
+++ 02)-angular/06-forms-app/src/app/reactive/pages/dinamic-page/dinamic-page.component.ts
@@ -1,9 +1,95 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators ,  FormControl} from '@angular/forms';
 
 @Component({
   templateUrl: './dinamic-page.component.html',
   styles: ``
 })
-export class DinamicPageComponent {
 
+export class DinamicPageComponent {
+// https://www.udemy.com/course/angular-fernando-herrera/learn/lecture/37405954#overview
+// https://v16.angular.io/guide/reactive-forms
+
+  // public myForm2 = new FormGroup({
+  //   favoriteGames: new FormArray([])
+  // });
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required, Validators.minLength(3) ]],
+    favoriteGames: this.fb.array([
+      ['Metal Gear', Validators.required ],
+      ['Death Stranding', Validators.required ],
+    ])
+  });
+
+  public newFavorite: FormControl = new FormControl('', Validators.required );
+
+  constructor( private fb: FormBuilder ) {}
+
+  get favoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+
+  isValidField( field: string ): boolean | null {
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched;
+  }
+
+  isValidFieldInArray( formArray: FormArray, index: number ) {
+    return formArray.controls[index].errors
+        && formArray.controls[index].touched;
+  }
+
+  getFieldError( field: string ): string | null {
+
+    if ( !this.myForm.controls[field] ) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+    console.log(errors )
+
+    for (const key of Object.keys(errors) ) {
+      switch( key ) {
+        case 'required':
+          return 'Este campo es requerido';
+
+        case 'minlength':
+          return `Mínimo ${ errors['minlength'].requiredLength } caracters.`;
+      }
+    }
+
+    return null;
+  }
+
+  onAddToFavorites():void {
+    /* este input esta afuera del control de formulario principal por lo que es un formControl*/
+    if ( this.newFavorite.invalid ) return;
+    const newGame = this.newFavorite.value;
+
+    // this.favoriteGames.push(  new FormControl( newGame, Validators.required ) );
+    this.favoriteGames.push(
+      this.fb.control( newGame, Validators.required )
+    );
+
+    this.newFavorite.reset();
+
+  }
+
+
+  onDeleteFavorite( index:number ):void {
+    this.favoriteGames.removeAt(index);
+  }
+
+  onSubmit():void {
+
+    if ( this.myForm.invalid ) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+    (this.myForm.controls['favoriteGames'] as FormArray ) = this.fb.array([]);
+    this.myForm.reset();
+
+  }
 }
