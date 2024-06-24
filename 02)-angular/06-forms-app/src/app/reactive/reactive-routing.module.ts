80175c14562488f236fc0403771053c02479c454
jhreyes483 <jhreyes483@misena.edu.co>
2024-06-24 10:23:31 -0500

    OJO CONFIGURACION DE RUTAS

--- 02)-angular/06-forms-app/src/app/reactive/reactive-routing.module.ts
+++ 02)-angular/06-forms-app/src/app/reactive/reactive-routing.module.ts
@@ -1,7 +1,33 @@
+import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BasicPageComponent } from './pages/basic-page/basic-page.component';
+import { DinamicPageComponent } from './pages/dinamic-page/dinamic-page.component';
+// https://www.udemy.com/course/angular-fernando-herrera/learn/lecture/37399880#overview
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'basic',
+        component: BasicPageComponent
+      },
+      {
+        path: 'dynamic',
+        component: DinamicPageComponent
+      },
+      {
+        path:'switches',
+        component: SwitchesPageComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'basic'
+      }
+    ]
 
-const routes: Routes = [];
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
